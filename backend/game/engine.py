39ed8691e16bf3336587e4b3b6099100552c9d84
epyuri <14885236+epyuri@user.noreply.gitee.com>
2026-04-27 23:57:17 +0800

    ctrl s

--- backend/game/engine.py
+++ backend/game/engine.py
@@ -6,6 +6,10 @@ from .player import Player
 
 class GameEngine:
     def __init__(self, user_ids: list):
+        """
+        游戏准备
+        :param user_ids:
+        """
         # 随机分配顺序
         random.shuffle(user_ids)
         self.player_order = user_ids
@@ -13,7 +17,8 @@ class GameEngine:
             uid: Player(uid, f"P{i + 1}") for i, uid in enumerate(user_ids)
         }
         # 设 P1 为初始庄家
-        self.banker_id = user_ids[0]
+        self.current_banker = user_ids[0]
+        self.next_banker = user_ids[1]
         self.player_ids = user_ids
         # 玩家行动顺序管理
         self.active_order = []  # 本回合行动顺序
@@ -22,23 +27,46 @@ class GameEngine:
         self.phase = "setup"
         self.current_round = 0  # 回合数
 
-    def prepare_new_round(self):
+    def start_new_round(self):
         """
-        回合开始时根据当前庄家设置本回合行动顺序
+        回合开始
         :return:
         """
+        # 回合数 + 1
         self.current_round += 1
-        idx = self.player_ids.index(self.banker_id)
-        self.active_order = self.player_ids[idx:] + self.player_ids[:idx]
+        # 预设下一位庄家
+        idx = self.player_ids.index(self.current_banker)
+        default_next_idx = (idx + 1) % len(self.player_ids)
+        self.next_banker = self.player_ids[default_next_idx]
+        # 生成本回合行动顺序
+        self.active_order = self.player_ids[idx:] + self.player_ids[:idx];
         self.turn_index = 0
+        # 回合开始时其他操作
+        #
+        #
+        #
+        #
+        #
 
-    def get_current_user_id(self):
-        return self.active_order[self.turn_index]
-
-    def set_next_banker_manually(self, user_id: int):
+    def finish_round(self):
         """
-        设定下回合庄家
-        :param user_id:
+        回合结束
         :return:
         """
-        self.banker_id = user_id
+        self.current_banker = self.next_banker
+        # 回合结束操作
+        #
+        #
+        #
+        #
+        #
+        # 下一回合开始
+        self.start_new_round()
+
+    def skill_steal_banker(self, thief_id: int):
+        """
+        切换 banker
+        :param thief_id:
+        :return:
+        """
+        self.next_banker = thief_id
