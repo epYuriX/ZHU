9f9c5440e64f3ec6ee552c281bafcc7a37002756
epyuri <207058982@qq.com>
2026-05-13 23:48:00 +0800

    ctrl s

--- backend/managers/room_manager.py
+++ backend/managers/room_manager.py
@@ -118,6 +118,8 @@ class RoomManager:
             if uid is not None:
                 if uid in self.user_room_map:
                     del self.user_room_map[uid]
+
+
         # 删除房间
         del self.rooms[rid]
 
